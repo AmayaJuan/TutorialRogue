cf29a166e2ee1a12979ef159ee389e5f2e04d5aa
AmayaJuan <jpva360@gmail.com>
2017-07-31 14:58:53 -0500

    Tutorial 04
    Generando los niveles 1 de 2

---
+++ Rogue/Assets/RogueProyecto/Script/BoardManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public GameObject[] floorTiles, outerWallTiles; //Losetas
+
+    Transform boardHolder;
+    int columns = 8;
+    int rows = 8;
+
+    public void SetupScene()
+    {
+        BoardSetup();
+    }
+
+    void BoardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;
+
+        for (int x = -1; x < columns + 1; x++)
+        {
+            for (int y = -1; y < rows + 1; y++)
+            {
+                GameObject toInstantiate = GetRandomInArray(floorTiles);
+
+                if (x == -1 || y == -1 || x == columns || y == rows)
+                    toInstantiate = GetRandomInArray(outerWallTiles);
+
+                GameObject instance = Instantiate(toInstantiate, new Vector2(x, y), Quaternion.identity);
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    GameObject GetRandomInArray(GameObject[] array)
+    {
+        return array[Random.Range(0, array.Length)];
+    }
+}
---
+++ Rogue/Assets/RogueProyecto/Script/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public BoardManager boardScript;
+
+    void Awake()
+    {
+        boardScript = GetComponent<BoardManager>();
+    }
+
+    void Start ()
+    {
+        InitGame();
+    }
+	
+    void InitGame()
+    {
+        boardScript.SetupScene();
+    }
+	
+	void Update ()
+    {
+		
+	}
+}
