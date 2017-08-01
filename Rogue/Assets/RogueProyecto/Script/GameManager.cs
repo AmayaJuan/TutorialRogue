98ef9781d3f739c7c96ba10cf081efe41162b710
AmayaJuan <jpva360@gmail.com>
2017-08-01 13:53:13 -0500

    Areglos
    areglando

--- Rogue/Assets/RogueProyecto/Script/GameManager.cs
+++ Rogue/Assets/RogueProyecto/Script/GameManager.cs
@@ -1,27 +1,14 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance;
-    public BoardManager boardScript;
-    [HideInInspector] public bool playersTurn = true;
-    public int playerFoodPoints = 100;
-    public float turnDelay = .1f;
-
-    bool enemiesMoving;
-    List<Enemy> enemies = new List<Enemy>();
+    public BoardMananager boardScript;
 
     void Awake()
     {
-        if (GameManager.instance == null)
-            GameManager.instance = this;
-        else if (GameManager.instance != this)
-            Destroy(gameObject);
-
-        DontDestroyOnLoad(gameObject);
-        boardScript = GetComponent<BoardManager>();
+        boardScript = GetComponent<BoardMananager>();
     }
 
     void Start ()
@@ -29,44 +16,8 @@ public class GameManager : MonoBehaviour
         InitGame();
     }
 
-    void Update()
-    {
-        if (playersTurn || enemiesMoving)
-            return;
-
-        StartCoroutine(MoveEnemies());
-    }
-
     void InitGame()
     {
-        enemies.Clear();
-        boardScript.SetupScene(3);
-    }
-	
-	public void GameOver()
-    {
-        enabled = false;
-    }
-
-    IEnumerator MoveEnemies()
-    {
-        enemiesMoving = true;
-        yield return new WaitForSeconds(turnDelay);
-
-        if (enemies.Count == 0)
-            yield return new WaitForSeconds(turnDelay);
-
-        for (int i = 0; i < enemies.Count; i++)
-        {
-            enemies[i].MoveEnemy();
-            yield return new WaitForSeconds(enemies[i].moveTime);
-        }
-        playersTurn = true;
-        enemiesMoving = false;
-    }
-
-    public void AddEnemyToList(Enemy enemy)
-    {
-        enemies.Add(enemy);
+        boardScript.SetupScene();
     }
 }
